9530a53f5795236ec16c536d5a60ef4b672f2e86
Armando <armandozabala@gmail.com>
2020-10-17 16:48:37 -0500

    UPDATE boilerplate 404

--- src/app/pages/notpagefound/notpagefound.component.ts
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotpagefoundComponent implements OnInit {
 
+
+  year = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit(): void {
